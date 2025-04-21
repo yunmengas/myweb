33ccd26092c1675ea90402a9998978e045dadd7a
yunmengas <772853301@qq.com>
2025-04-21 15:59:59 +0800

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementById('godyjx').addEventListener('click', function () {
+  //window.location.href = 'https://www.123pan.com/s/iuk0jv-i3V63.html'; // 注意：这里添加了https://前缀，因为大多数现代网站都使用HTTPS协议  
+  window.location.href = 'index2.html';
+
+
+});
